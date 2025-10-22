9772b18eea414128bfe2dc88366220d1a30e6e84
Sharun <sharunsankar@gmail.com>
2025-10-22 20:55:07 +0530

    [Optimisation] image size reduced

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true, // since static export can’t use next/image optimization
+    },
+    trailingSlash: true,
+};
 
 export default nextConfig;
